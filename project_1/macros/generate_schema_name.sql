ed3ec4a1322627b6ff7775a7c1cce71b50ae98ab
Tyler Rouze <tyler.rouze@fishtownanalytics.com>
2024-08-22 19:17:11 +0000

    update schema maco

---
+++ project_1/macros/generate_schema_name.sql
@@ -0,0 +1,26 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if target.name=='prod' -%}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ custom_schema_name | trim }}
+
+        {%- endif -%}
+    {%- else -%}
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+
+        {%- else -%}
+
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+
+        {%- endif -%}
+    {%- endif -%}
+
+{%- endmacro %}
